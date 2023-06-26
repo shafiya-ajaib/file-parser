c4519770dd7c5d3d23e0df0516a1b71cde6df144
Shafiya Adzhani <shafiya.adzhani@ajaib.co.id>
2023-06-26 18:23:46 +0800

    refactor: change subscribe to block for error handling in create ipo order service

--- src/main/java/com/shafiya/fileparser/service/CreateIpoOrderServiceImpl.java
+++ src/main/java/com/shafiya/fileparser/service/CreateIpoOrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.shafiya.fileparser.service;
 import com.shafiya.fileparser.entity.IpoOrder;
 import com.shafiya.fileparser.repository.IpoOrderRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class CreateIpoOrderServiceImpl implements CreateIpoOrderService {
@@ -12,10 +13,13 @@ public class CreateIpoOrderServiceImpl implements CreateIpoOrderService {
         this.ipoOrderRepository = ipoOrderRepository;
     }
 
+
     @Override
     public void saveIfNotExist(IpoOrder ipoOrder) {
         this.ipoOrderRepository.findBySid(ipoOrder.getSid())
-                .switchIfEmpty(this.ipoOrderRepository.save(ipoOrder))
-                .subscribe();
+                .switchIfEmpty(this.ipoOrderRepository.save(ipoOrder)
+                        .onErrorMap(throwable -> new RuntimeException("Error saving order", throwable)))
+                .onErrorMap(throwable -> new RuntimeException("Error finding order", throwable))
+                .block();;
     }
 }
